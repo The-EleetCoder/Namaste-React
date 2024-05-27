22095499f479fb15211216bf3018d013cc809f4a
The-EleetCoder <jaijain1803@gmail.com>
2024-05-27 12:05:07 +0530

    day 2

--- 1_inception/App.js
+++ 1_inception/App.js
@@ -1,4 +1,33 @@
 // this {} is the place where we give attributes to the corresponding tag
-const heading = React.createElement('h1',{id:'heading'},'Hello, world! using React');
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello, world! using React"
+);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+
+// Creating nested elements using react
+{
+  /* 
+<div id='parent'>
+    <div id='child'>
+        <h1>this is h1 tag</h1>
+    </div>
+</div> 
+*/
+}
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "this is h1 tag")
+  )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
