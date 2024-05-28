e707f1065ccda4cb39fa5013dc182efcbc30f8da
The-EleetCoder <jaijain1803@gmail.com>
2024-05-28 11:23:25 +0530

    day 3

--- 3_laying_the_foundation/App.js
+++ 3_laying_the_foundation/App.js
@@ -1,2 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// react element
+const heading = React.createElement("h1",{id: "heading"}, "Namaste React 🚀");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
