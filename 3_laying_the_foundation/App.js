4b11113553d3f31d5f96e194df65c9d755283eba
The-EleetCoder <jaijain1803@gmail.com>
2024-05-28 12:00:17 +0530

    day 3

--- 3_laying_the_foundation/App.js
+++ 3_laying_the_foundation/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// react element
+// react element using core react
 const heading = React.createElement("h1",{id: "heading"}, "Namaste React 🚀");
+
+// react element using jsx
+const jsxHeading = <h1 id='heading'>Namaste React using jsx 🚀</h1>
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
