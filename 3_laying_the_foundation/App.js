99d3282b4dbc12bcea5d91228c0b14ae010391e2
The-EleetCoder <jaijain1803@gmail.com>
2024-05-28 11:03:13 +0530

    day 3

--- 3_laying_the_foundation/App.js
+++ 3_laying_the_foundation/App.js
@@ -1,14 +1,2 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "this is h1 tag"),
-    React.createElement("h2", {}, "this is h2 tag"),
-  ])
-);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
