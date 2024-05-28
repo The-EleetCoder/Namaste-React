bab9bc6bf1e8e91c29b480a5658fa50ec887c911
The-EleetCoder <jaijain1803@gmail.com>
2024-05-28 22:19:03 +0530

    day 3

--- 3_laying_the_foundation/App.js
+++ 3_laying_the_foundation/App.js
@@ -2,10 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // react element using core react
-const heading = React.createElement("h1",{id: "heading"}, "Namaste React 🚀");
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Namaste React 🚀"
+);
 
 // react element using jsx
-const jsxHeading = <h1 id='heading'>Namaste React using jsx 🚀</h1>
+const jsxHeading = <h1 id="heading">Namaste React using jsx 🚀</h1>;
+
+// react functional component
+const Title = () => {
+  return <h1>Title component 🚀</h1>;
+};
+
+const HeadingComponent = () => {
+  return (
+    <>
+      <Title />
+      <h1>Namaste React functional component 🚀</h1>
+    </>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent/>);
