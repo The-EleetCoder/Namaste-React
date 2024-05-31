1c4e66cb1097d1543a4fc076bc1f1b77977dee10
The-EleetCoder <jaijain1803@gmail.com>
2024-05-31 18:28:40 +0530

    day 6

---
+++ 5_lets_get_hooked/src/components/Body.js
@@ -0,0 +1,17 @@
+import RestaurantCard from "./RestaurantCard";
+import { resObj } from "../utils/mockData";
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        {resObj.restaurants.map((restaurant) => {
+          return <RestaurantCard key={restaurant.info.id} data={restaurant} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
---
+++ 5_lets_get_hooked/src/utils/constants.js
@@ -0,0 +1,5 @@
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL =
+  "https://logos-world.net/wp-content/uploads/2020/11/Swiggy-Symbol.png";
