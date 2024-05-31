30e0a5731275bbd68c9c92adb0633b6f37ee7d52
The-EleetCoder <jaijain1803@gmail.com>
2024-05-31 19:27:56 +0530

    day 6

--- 5_lets_get_hooked/src/components/Body.js
+++ 5_lets_get_hooked/src/components/Body.js
@@ -4,7 +4,10 @@ import { resObj } from "../utils/mockData";
 const Body = () => {
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="body-top">
+        <div className="search">Search</div>
+        <button className="filter-btn">Top Rated Restaurants</button>
+      </div>
       <div className="res-container">
         {resObj.restaurants.map((restaurant) => {
           return <RestaurantCard key={restaurant.info.id} data={restaurant} />;
