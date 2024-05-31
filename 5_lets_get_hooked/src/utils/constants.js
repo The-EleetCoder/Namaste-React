331a4b1ed38233f0d2a9eb6d51fe0db7f9968aee
The-EleetCoder <jaijain1803@gmail.com>
2024-05-31 20:07:59 +0530

    day 6

--- 5_lets_get_hooked/src/utils/constants.js
+++ 5_lets_get_hooked/src/utils/constants.js
@@ -2,4 +2,4 @@ export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const LOGO_URL =
-  "https://logos-world.net/wp-content/uploads/2020/11/Swiggy-Symbol.png";
+  "https://upload.wikimedia.org/wikipedia/en/thumb/1/12/Swiggy_logo.svg/2560px-Swiggy_logo.svg.png";
