5ff3921d9aec20f8a39bb8e2bc962ae3ce918ef5
The-EleetCoder <jaijain1803@gmail.com>
2024-06-01 13:52:35 +0530

    day 7

--- 6_exploring_the_world/src/components/Body.js
+++ 6_exploring_the_world/src/components/Body.js
@@ -5,15 +5,19 @@ import Shimmer from "./Shimmer";
 const Body = () => {
   const [restaurantList, setRestaurantList] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchData();
-  },[]);
+  }, []);
 
-  const fetchData = async ()=>{
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65420&lng=77.23730&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65420&lng=77.23730&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
     const json = await data.json();
-    setRestaurantList(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
-  }
+    setRestaurantList(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
 
   const handleFilter = () => {
     let filteredList = restaurantList.filter((res) => res?.info.avgRating > 4);
@@ -21,11 +25,9 @@ const Body = () => {
     setRestaurantList(filteredList);
   };
 
-  if (restaurantList && restaurantList.length == 0){
-    return <Shimmer/>
-  }
-
-  return (
+  return (restaurantList && restaurantList.length) == 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="body-top">
         <div className="search">Search</div>
