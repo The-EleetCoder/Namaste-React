e50a3e4b0bf66abb35a20e2b44b2ddee27efcaaf
The-EleetCoder <jaijain1803@gmail.com>
2024-06-02 12:19:20 +0530

    day 8

--- 7_finding_the_path/src/App.js
+++ 7_finding_the_path/src/App.js
@@ -4,26 +4,33 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 
 const AppLayout = () => {
   return (
     <div className="app">
-      <Header/>
-      <Body/>
+      <Header />
+      <Body />
     </div>
   );
 };
 
 const appRouter = createBrowserRouter([
   {
-    path: '/',
-    element: <AppLayout/>,
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error/>
   },
-      {
-        path: '/about',
-        element: <About/>
-      }
-])
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={appRouter} />);
