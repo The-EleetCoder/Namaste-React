a4f3e6bc55a42c1259e3e425a8c128ca666f4d8d
The-EleetCoder <jaijain1803@gmail.com>
2024-06-03 17:17:45 +0530

    day 9

--- 7_finding_the_path/src/components/RestaurantMenu.js
+++ 7_finding_the_path/src/components/RestaurantMenu.js
@@ -1,40 +1,73 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import RestaurantMenuCard from "./RestaurantMenuCard";
+import {
+  RESTAURANT_MENU_API_URL,
+  RESTAURANT_MENU_TYPE_KEY,
+} from "../utils/constants";
 
 const RestaurantMenu = () => {
-  const [restaurantMenudata, setRestaurantMenudata] = useState(null);
-
   const { resId } = useParams();
-
-  const recMenuList =
-    restaurantMenudata?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(
-      (card) =>
-        card?.card?.card["@type"] ==
-          "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory" &&
-        card?.card?.card?.title == "Recommended"
-    )[0].card?.card?.itemCards;
+  const [restaurantData, setRestaurantData] = useState(null);
+  const [restaurantMenuList, setRestaurantMenuList] = useState([]);
 
   useEffect(() => {
     fetchMenu();
   }, []);
 
   const fetchMenu = async () => {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.65420&lng=77.23730&restaurantId=" +
-        resId
-    );
-    const json = await data.json();
-    setRestaurantMenudata(json.data);
+    try {
+      const data = await fetch(RESTAURANT_MENU_API_URL + resId);
+      const json = await data.json();
+
+      setRestaurantMenuList(
+        json?.data.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(
+          (card) =>
+            card?.card?.card["@type"] == RESTAURANT_MENU_TYPE_KEY &&
+            card?.card?.card?.title == "Recommended"
+        )[0].card?.card?.itemCards
+      );
+
+      setRestaurantData(json?.data?.cards[2]?.card?.card?.info);
+    } catch (error) {
+      setRestaurantData(null);
+      setRestaurantMenuList([]);
+      console.log(error);
+    }
   };
 
   return (
-    <div className="menu" style={{ marginTop: "5em" }}>
-      <h1>{restaurantMenudata?.cards[2]?.card?.card?.info?.name}</h1>
-      <h2>Recommended</h2>
-      {recMenuList?.map((item) => {
-        return <RestaurantMenuCard key={item?.card?.info?.id} data={item?.card?.info} />;
-      })}
+    <div className="restaurant-menu-wrapper">
+      <div className="restaurant-menu-top-div">
+        <div className="restaurant-menu-name">{restaurantData?.name}</div>
+        <div className="restaurant-menu-details">
+          <div className="restaurant-menu-rating">
+            {`${restaurantData?.avgRating} (${restaurantData?.totalRatingsString})`}
+          </div>
+          <div className="restaurant-menu-cuisines">
+            {restaurantData?.cuisines?.join(", ")}
+          </div>
+          <div className="restaurant-menu-time">
+            {restaurantData?.sla?.slaString}
+          </div>
+          <div className="restaurant-menu-distance-fee">
+            {`${restaurantData?.sla?.lastMileTravelString} | ₹${
+              restaurantData?.feeDetails?.totalFee / 100
+            } delivery fee will apply`}
+          </div>
+        </div>
+      </div>
+
+      <div className="restaurant-menu-bottom-div">
+        {restaurantMenuList?.map((item) => {
+          return (
+            <RestaurantMenuCard
+              key={item?.card?.info?.id}
+              data={item?.card?.info}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
