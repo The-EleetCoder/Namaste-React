2d88d16ebb2523d3139842220912cc4cf282206c
The-EleetCoder <jaijain1803@gmail.com>
2024-06-04 15:09:58 +0530

    day 10

--- 8_lets_get_classy/src/components/DeveloperClass.js
+++ 8_lets_get_classy/src/components/DeveloperClass.js
@@ -3,12 +3,26 @@ import React from "react";
 export default class DeveloperClass extends React.Component {
   constructor(props) {
     super(props);
-    console.log(props);
+
+    this.state = {
+      count: 0,
+    };
   }
   render() {
     const { name, location, contact } = this.props;
+    const { count } = this.state;
     return (
       <div>
+        <p>Count: {count}</p>
+        <button
+          onClick={() => {
+            this.setState({
+              count: this.state.count + 1,
+            });
+          }}
+        >
+          Count increase
+        </button>
         <p>Name: {name}</p>
         <p>Location: {location}</p>
         <p>Contact: {contact} </p>
